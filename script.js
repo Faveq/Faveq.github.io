f066b0e537616bf4158430fb6e456f4e81a9b6ac
Faveq <58594037+Faveq@users.noreply.github.com>
2020-11-01 13:32:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function losuj(){
+
+let x = Math.floor(Math.random() * 10);
+
+if(x==0){
+    document.getElementById("liczba").innerHTML = ",,Twój czas jest ograniczony, więc nie marnuj go na bycie kimś, kim nie jesteś” ~ STEVE JOBS"
+}if(x==1){
+    document.getElementById("liczba").innerHTML = ",,Bądź takim rodzajem człowieka, że gdy Twoja stopa dotyka podłogi, gdy rano wstajesz, diabeł w piekle mówi „Ooo kurwa, wstał…”” ~ Dwayne Johnson"
+}if(x==2){
+    document.getElementById("liczba").innerHTML = ",,Kto ma cierpliwość, będzie miał, co zechce.” ~ Abraham Lincoln"  
+}
+if(x==3){
+    document.getElementById("liczba").innerHTML = ",,Poddający się – nigdy nie wygrywa, a wygrywający – nigdy się nie poddaje.” ~ Napoleon Hill"  
+}if(x==4){
+    document.getElementById("liczba").innerHTML = ",,Ci, którzy są wystarczająco szaleni, by myśleć, że są w stanie zmienić świat, są tymi, którzy go zmieniają.” ~ Steve Jobs"  
+}if(x==5){
+    document.getElementById("liczba").innerHTML = ",,Najlepszą zemstą jest ogromny sukces” ~ FRANK SINATRA"  
+}if(x==6){
+    document.getElementById("liczba").innerHTML = ",,Jeśli nie czujesz się godny, by wyrosły Ci skrzydła, nigdy nie oderwiesz się od ziemi.” ~ Nick Vujicic"  
+}if(x==7){
+    document.getElementById("liczba").innerHTML = ",,Kto potrafi cieszyć się z małych rzeczy, mieszka w ogrodzie pełnym szczęśliwości.” ~ Phil Bosmans"  
+}if(x==8){
+    document.getElementById("liczba").innerHTML = ",,Nigdy nie rezygnuj z celu tylko dlatego, że osiągnięcie go wymaga czasu. Czas i tak upłynie.” ~ H. Jackson Brown"  
+}if(x==9){
+    document.getElementById("liczba").innerHTML = ",,Mądrzy ciągle się uczą, głupcy najczęściej wszystko umieją.” ~ Apolinary Despinoix"  
+}if(x==10){
+    document.getElementById("liczba").innerHTML = ",,Nie pozwól, by ograniczona wyobraźnia innych była Twoim ograniczeniem” ~ Mae Jemison"  
+}
+
+
+}
