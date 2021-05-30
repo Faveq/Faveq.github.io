abfc38b7e271f176f32ae44d43b6f4638e4355a1
Faveq <58594037+Faveq@users.noreply.github.com>
2021-05-30 14:55:17 +0200

    Delete E 14-05-17 06 directory

--- E 14-05-17 06/script.js
+++
@@ -1,9 +0,0 @@
-function Oblicz(){
-	var a = document.getElementById("a").value;
-	var b = document.getElementById("b").value;
-	var pole = 2 * (a * 2.7) + 2 * (b * 2.7);
-	var cena = pole * 8;
-	document.getElementById("Pole").innerHTML = "Powierzchnia całkowita ścian: " + pole;
-	document.getElementById("Wynik").innerHTML = "Koszt malowania: " + cena + " zł";
-	
-}
